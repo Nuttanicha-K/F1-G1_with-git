8b392a6f5dcc8f55f8bca3236673c874d7d38afd
Nuttanicha Kumhan <nattanichakumhan@gmail.com>
2025-04-11 11:38:10 +0700

    เพิ่มยาเคmean เฉย ๆ

--- For_working.py
+++ For_working.py
@@ -6,7 +6,6 @@ import pandas as pd
 import fastf1.plotting
 import numpy as np
 import sklearn
-from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 #%%
 #ขอลองเอาดาต้า1แมตช์มาทดลองทำเป็นโมเดลเล็ก ๆ ก่อนนะแม่
